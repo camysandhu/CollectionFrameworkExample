f556438b20c7b04bfbfe79e2aaf1d4410a995bf2
MacStudent <macstudent@126-iMac36.local>
2019-09-24 18:25:50 -0400

    array

---
+++ src/main/java/com/lambton/CollectionFrameworkExample.java
@@ -0,0 +1,34 @@
+package com.lambton;
+
+import java.util.ArrayList;
+
+public class CollectionFrameworkExample
+{
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> mStringArrayList = new ArrayList<>();
+        mStringArrayList.add("A");
+        mStringArrayList.add("B");
+        mStringArrayList.add("C");
+        mStringArrayList.add("C");
+        mStringArrayList.add("A");
+        mStringArrayList.add("A");
+        mStringArrayList.add("A");
+        mStringArrayList.add("A");
+        mStringArrayList.add("A");
+
+       // System.out.println(mStringArrayList);
+        int i;
+        for(i=0;i<=mStringArrayList.size()-1;i++)
+        {
+            System.out.println(mStringArrayList.get(i));
+        }
+
+
+
+
+
+
+    }
+}
